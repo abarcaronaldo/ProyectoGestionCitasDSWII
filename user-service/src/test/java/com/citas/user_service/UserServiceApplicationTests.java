b632aa471ef35eeef83c33afbc618ed4be9aea2c
Ronaldo <abarcaronaldo18@gmail.com>
2026-05-27 17:02:39 -0500

    Estructura inicial

---
+++ user-service/src/test/java/com/citas/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citas.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
