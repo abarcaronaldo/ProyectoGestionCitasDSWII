16f2bcb7e5256550ccef0706dbf4c60a64691d01
JpSiesquen <jpsiesquen@gmail.com>
2026-06-18 11:02:12 -0400

    feat(historial): configuracion base de historial-medico-service - Closes #37

---
+++ backend/historial-medico-service/src/test/java/com/gestioncitas/historial_medico_service/HistorialMedicoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestioncitas.historial_medico_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorialMedicoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
